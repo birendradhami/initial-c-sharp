47547e054be47ea7c6774b4be08932b7c3f9e4ec
birendradhami <sbirendradhami@gmail.com>
2023-05-19 18:06:21 +0545

    Add project files.

---
+++ Class.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/*public class Shop
+{
+
+    //Accessing fields and methods
+    /*public string name;
+
+	public string getName()
+	{
+		name = Console.ReadLine();
+		return name;
+
+	}
+
+	public void printName()
+	{
+		Console.WriteLine(name);
+	}*/
+
+	//Constructor
+	/*public Shop()
+	{
+		Console.WriteLine("Hamro Pasal");
+	}*/
+
+	
+//Inheritance
+	/* class Student
+	{
+	  public string Name;
+	}
+	class Info : Student
+	{
+    public void printName()
+    {
+        Name = Console.ReadLine();
+    }
+}*/
+
+public class Shop
+{
+	public string Name;
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+     internal class Program
+    {
+        static void Main()
+        {
+            // Hello World 
+            /*Console.WriteLine("Birendra Dhami");
+            Console.ReadLine();*/
+
+            //User input
+            /*Console.WriteLine("Enter Your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);*/
+
+
+            //jarray 2D
+            /*string[][,] name = new string[2][,] {
+                new string[,] {{ "Ram" , "Hari"} , { "Me", "You" } },
+                new string[,] {{ "Sita" , "Gita"} , {"Me" , "You"} }
+            };
+
+            for(int i=0; i< name.Length;  i++)
+            {
+                for(int j=0; j< name[i].GetLength(0); j++)
+                    {
+                    for(int k=i; k< name[i].GetLength(1); k++)
+                    {
+                        Console.WriteLine(name[i][j,k]);
+                    }
+                }
+            }*/
+
+
+            //Creating object
+            /*  Console.WriteLine("Enter Name of the Shop:");
+
+              Shop s1 = new Shop();
+              s1.name = Console.ReadLine();
+              Console.WriteLine(s1.name);*/
+
+
+            //Accessing fields and methods
+            /* Console.WriteLine("Enter Name of the Shop:");
+
+             Shop s1 = new Shop();
+             s1.getName();
+             s1.printName();*/
+
+            //Constructor 
+            //Shop s1 = new Shop();
+
+
+            //Inheritance
+            /* Info i1 = new Info();
+             i1.printName();*/
+
+            Console.WriteLine("Enter Shop's name: ");
+            Shop s1 = new Shop();
+            s1.Name = Console.ReadLine();
+            Console.WriteLine(s1.Name);
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
